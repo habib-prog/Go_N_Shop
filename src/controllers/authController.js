74a9baa3b136cc287174b97d53acb6a975109414
habib-prog <xavierjames701@gmail.com>
2026-05-12 02:53:57 +0600

    Zod Validation moved to Helpers

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,5 @@
+const { signUpSchema } = require("../helpers/ZodValidators/validator");
 const User = require("../models/userSchema");
-const { z } = require("zod");
-// ZOD shcema define
-const signUpSchema = z.object({
-  fullname: z.string().min(3, "Full name must be at least 3 characters"),
-  email: z.string().email("Invalid email"),
-  password: z.string().min(6, "Password must be minimum 6 charecters"),
-  avatar: z.string().optional(),
-  address: z.string().optional(),
-});
 
 const signUp = async (req, res) => {
   const parsed = signUpSchema.safeParse(req.body);
---
+++ src/helpers/ZodValidators/validator.js
@@ -0,0 +1,11 @@
+const { z } = require("zod");
+// ZOD shcema define
+const signUpSchema = z.object({
+  fullname: z.string().min(3, "Full name must be at least 3 characters"),
+  email: z.string().email("Invalid email"),
+  password: z.string().min(6, "Password must be minimum 6 charecters"),
+  avatar: z.string().optional(),
+  address: z.string().optional(),
+});
+
+module.exports = { signUpSchema };
